ad1261945e5223dc45abf9498f3da50ed3221ef7
dtg57 <96553602+dtg57@users.noreply.github.com>
2023-03-31 12:19:30 +0100

    Update H0_posterior_bright.py
    Now processing more than one bright run samples file to produce combined H0 likelihood

--- recover_H0_bright/H0_posterior_bright.py
+++ recover_H0_bright/H0_posterior_bright.py
@@ -19,21 +19,36 @@ H0_max = 100
 H0_step = 0.5
 H0_n = int((H0_max - H0_min) / H0_step)
 
-samples_file = 'samples_bright.json'
+# folder where runs are stored, append / to this
+samples_folder = 'results/'
+samples_files = listdir(samples_folder)
+posterior_samples = {}
 
-with open(samples_file) as f:
-    posterior_samples = json.load(f)["posterior"]["content"]
+for samples_file in samples_files:
+    with open(samples_folder + samples_file) as f:
+        posterior_samples[samples_file] = json.load(f)["posterior"]["content"]
 
-luminosity_distance = np.array(posterior_samples['luminosity_distance'])
-H0 = z * c / luminosity_distance
-print(len(H0))
+H0_likelihood_all = {}
+H0_likelihood_combined = np.ones(H0_n)
+for samples_file in posterior_samples:    
+    luminosity_distance = np.array(posterior_samples[samples_file]['luminosity_distance'])
+    H0 = z * c / luminosity_distance
+    print(len(H0))
+    H0_likelihood, bin_edges = np.histogram(H0, np.arange(H0_min, H0_max + H0_step, H0_step))
+    # normalise so likelihood integrates to 1
+    H0_likelihood = H0_likelihood / (H0_step * sum(H0_likelihood))
+    H0_likelihood_all[samples_file] = H0_likelihood
+    H0_likelihood_combined = H0_likelihood_combined * H0_likelihood
+    figure = plt.figure(samples_file)
+    ax = figure.add_axes([0.15, 0.1, 0.8, 0.8])
+    plt.title('H0 posterior from 1 bright run: ' + samples_file)
+    ax.set_ylim([0, max(H0_posterior * 1.1)])
+    plt.stairs(H0_posterior, bin_edges)
 
-posterior_H0, bin_edges = np.histogram(H0, np.arange(H0_min, H0_max + H0_step, H0_step))
-
-figure = plt.figure()
+figure = plt.figure('all')
 ax = figure.add_axes([0.15, 0.1, 0.8, 0.8])
-plt.title('H0 posterior from 1 bright run')
-ax.set_ylim([0, max(posterior_H0 * 1.1)])
-plt.stairs(posterior_H0, bin_edges)
+plt.title('H0 posterior from 1 bright run: combined')
+ax.set_ylim([0, max(H0_likelihood_combined * 1.1)])
+plt.stairs(H0_likelihood_combined, bin_edges)
 
 plt.show()
